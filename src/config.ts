a2294aed4cdada245a91a3a288df8fc7e06c45fe
Navya Kaushik <navyak8124@gmail.com>
2025-12-12 19:36:02 +0530

    Add orders table

--- src/config.ts
+++ src/config.ts
@@ -22,12 +22,28 @@ export const redisClient = process.env.REDIS_URL
 export async function checkConnections() {
     try {
         const pgRes = await pgPool.query('SELECT NOW()');
-        console.log(' Postgres Connected');
+        console.log('Postgres Connected');
+        
+        await pgPool.query(`
+            CREATE TABLE IF NOT EXISTS orders (
+                id SERIAL PRIMARY KEY,
+                order_id VARCHAR(50) UNIQUE NOT NULL,
+                amount DECIMAL NOT NULL,
+                dex VARCHAR(20),
+                price DECIMAL,
+                tx_hash VARCHAR(100),
+                status VARCHAR(20),
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            );
+        `);
+        console.log('Table "orders" verified/created');
+        // 
+
         await redisClient.ping();
         console.log('Redis Connected');
         return true;
     } catch (error) {
-        console.error('Connection Error:', error);
+        console.error(' Connection Error:', error);
         return false;
     }
 }
